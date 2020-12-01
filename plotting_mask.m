3aefa07d0255317166f690ad62976690f9e900c9
helenwl <43169711+helenwl@users.noreply.github.com>
2020-12-01 14:56:54 +0000

    Update plotting_mask.m
    fixed scaling error

--- plotting_mask.m
+++ plotting_mask.m
@@ -2,11 +2,11 @@ for i= 0:9
     csvdata = readtable(sprintf('defectiveMoS2lattice_cellholes_%.f.csv', i), 'HeaderLines', 0);
     disp(sprintf('helloround%f',i))
     lattice= table2array(csvdata(:,:));
-    figure1= scatter(lattice(1:end,2), lattice(1:end,3));
-    set(gca, 'YDir','reverse', 'xtick', [], 'ytick', [], 'color', 'None');
+    figure1= scatter(10.24*lattice(1:end,2), 10*lattice(1:end,3));
+    set(gca, 'YDir','reverse', 'color', 'None');
     colormap gray;
     axis image;
-    xlim([0,100]);
-    ylim([0,100]);
+    xlim([0,1000]);
+    ylim([0,1000]);
     saveas(figure1, sprintf('defectiveMoS2lattice_cellholes_%.f.png', i))
 end
